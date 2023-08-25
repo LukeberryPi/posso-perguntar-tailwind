09df28fa7b76b9b3f5501d3eb11f520b9bda48a5
Luke Berry <lukeberrypi@Lukes-MacBook-Pro.local>
2023-08-25 14:06:29 -0300

    configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        berryBlue: "#79cbe3",
+        navyDarker: "#00151f",
+        navyDark: "#002232",
+        blueLight: "#c3d4db",
+        blueLighter: "#e5f9ff",
+      },
+    },
+  },
+  plugins: [],
+};
